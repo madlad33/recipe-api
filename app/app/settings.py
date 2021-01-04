e311e483391bd4b63c01a1771c3c99ebaeb70f86
Tanmay <tanmay.milky33@gmail.com>
2021-01-04 17:20:27 +0530

    Secret key changed

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .secret_settings import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fe2*oabzvr8z^9(%^0&)!pd4(gjyrj*8_-mdr69p6t7&647p_+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
